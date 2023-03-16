5197dde73d6e5a0f3b1938a949ecec7238904378
BasilePajot <91027316+BasilePajot@users.noreply.github.com>
2023-03-16 16:45:22 +0100

    Update Fasta_maker.sh

--- 4-Making_a_fasta_file/Fasta_maker.sh
+++ 4-Making_a_fasta_file/Fasta_maker.sh
@@ -1,15 +1,20 @@
 #! /bin/sh
 
-# Importing variables
+# This script takes the csv files in the parallel folder and creates a fasta file with the data.
 
+# Variable importation. It takes the path of the parallel folder.
 FOLDER=$1
 
+# We will iterate over the files in the parallel folder to isolate the csv files containing the filtered data.
 for FILE in $(ls $FOLDER | grep '.csv'); do
 	
+	# First, we have to create a path to the file from the parallel folder to be accessed later. We also extract the order from the name of the file.
 	PATH_FILE="${FOLDER}${FILE}"
 	ORDER=$(echo "${FILE}" | cut -d'_' -f2)
-	echo ${ORDER}
+	echo "${ORDER}"
 	
+	# Then, we will iterate over the information in the file to place it in several temporary files (1 per information per order).
+	# Therefore, this loop creates 4 files per order.
 	for INFO in ID Classification Sequence Folding_data; do
 		if [[ "${INFO}" = "ID" ]]; then
 			cut -d';' -f1 ${PATH_FILE} > ${ORDER}_${INFO}.csv
@@ -26,9 +31,14 @@ for FILE in $(ls $FOLDER | grep '.csv'); do
 		fi
 	done
 	
+	# Once the temporary files are created, we firt paste the files containing the ID, classification and folding data together and replace
+	# the spaces in between each information by some semi-colons to be able to extract them more easily if needed. We then add a ">" at 
+	# the beginning of the line to separate the information from one sequence and the next. We store this in another temporary file.
 	paste ${ORDER}_ID.csv ${ORDER}_Classification.csv ${ORDER}_Folding_data.csv | sed 's/\t/;/g' | sed 's/^/>/g' > ${ORDER}_temporary.txt
+	# Finally, we paste the sequence to the file before and replace the spaces with a new line to separate the sequence from the information.
+	# We then take out the 2 first lines to remove the header of the csv files and only keep the sequences. We store the end product in a fasta file.
 	paste ${ORDER}_temporary.txt ${ORDER}_S* | sed 's/\t/\n/g' | tail -n+3 > ${ORDER}_sequences.fasta
 done
 
-# Cleaning up
+# Removing the construction files to only keep the fasta files from each order.
 rm *.csv *.txt
