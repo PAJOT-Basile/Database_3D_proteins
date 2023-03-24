57b2610e0f45dfa7219c96a62e888232c62191fd
BasilePajot <91027316+BasilePajot@users.noreply.github.com>
2023-03-24 19:46:39 +0100

    Update Isolating_sequences.py

--- 3-Filtering_gene_families_per_kingdom/Isolating_sequences.py
+++ 3-Filtering_gene_families_per_kingdom/Isolating_sequences.py
@@ -4,7 +4,8 @@ from pathlib import Path
 import shutil as shu
 
 # Data path
-directory_path = "../2-Extracting_families_per_kingdom"
+#directory_path = "../2-Extracting_families_per_kingdom"
+directory_path = "../0-test"
 list_reference_files = [file for file in os.listdir(directory_path) if file.endswith('.mne')]
 
 
@@ -16,6 +17,7 @@ class Extractor:
         self.extracting_family = None
     
     def check_sequence(self, reference_sequence, sequence_from_family_file):
+        
         if "".join([">", sequence_from_family_file]) == reference_sequence:
             return(True)
         else:
@@ -43,6 +45,7 @@ class Extractor:
 
 
 
+
 extractor = Extractor()
 extractor.init()
 
@@ -50,12 +53,14 @@ extractor.init()
 # Final loop
 for reference_file in list_reference_files:
     directory = reference_file.split("_")[0]
-    print(directory)
     line_nb = 1
-    for reference_line in reference_file:
-        for file in directory:
-            family_name = file.split("_")[0].strip("#")
-            for line in file:
+    ref_file = open(os.path.join(directory_path, reference_file), "r")
+    print(directory)
+    for reference_line in ref_file:
+        for file in os.listdir(os.path.join(directory_path, directory)):
+            family_name = file.split("#")[1]
+            file_to_read = open(os.path.join(directory_path, directory, file), "r")
+            for line in file_to_read:
 
                 if line.startswith(">") and extractor.extracting:
                     extractor.init()
@@ -63,11 +68,13 @@ for reference_file in list_reference_files:
                 if line.startswith(">"):
                     if extractor.check_sequence(reference_line, line):
                         extractor.extract_sequence_name(line, directory, family_name)
+                        print(f"Match for {family_name} in {directory}.")
                 
                 if not line.startswith(">") and extractor.extracting:
                     extractor.extracting_sequence_line(line, directory, family_name)
+                    #print(f"Extracting sequence for {family_name}.")
 
-        if line_nb % 500000 == 0:
+        if line_nb % 100 == 0:
             print(line_nb)
 
         line_nb += 1
