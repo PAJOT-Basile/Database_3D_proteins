e4591de6fec18553e16f2e741b966dbdd5deb8db
BasilePajot <91027316+BasilePajot@users.noreply.github.com>
2023-03-27 11:37:16 +0200

    Update Array_maker.sh

--- 3-Filtering_gene_families_per_kingdom/Array_maker.sh
+++ 3-Filtering_gene_families_per_kingdom/Array_maker.sh
@@ -8,15 +8,15 @@ for ORDER in $LIST_ORDERS; do
     #mkdir $ORDER
     File_number=1
     echo $ORDER
-    LIST_FAMILIES=$(ls "${DATA_PATH}${ORDER}")
-    for FAMILY in $LIST_FAMILIES; do
-        
-        bash Python_launcher.sh $DATA_PATH $ORDER $FAMILY 
-        
-        File_number=$(( $File_number + 1 ))
 
-        if [[ $(expr $File_number % 1000) -eq 0 ]]; then
-            echo $File_number
-        fi
+
+    LIST_FAMILIES=$(ls "${DATA_PATH}${ORDER}")
+
+    set -- $LIST_FAMILIES
+    while (( $# )); do
+        for ((i=0; i<10; i++)); do
+            [[ $1 ]] && bash Python_launcher.sh $DATA_PATH $ORDER "$1" & shift
+        done
+        wait
     done
 done
