c61aeb9ea1848abecaed5d61bbd49ec302f35f9b
BasilePajot <91027316+BasilePajot@users.noreply.github.com>
2023-03-29 14:37:20 +0200

    Update launcher.sh

--- 3-Filtering_gene_families_per_kingdom/launcher.sh
+++ 3-Filtering_gene_families_per_kingdom/launcher.sh
@@ -1,15 +1,14 @@
-#! /bin/sh
-
-
-# This script takes into account the path to the previous directory and the number of jobs to run in parallel.
+# This script takes into account the path to the previous directory.
 DATA_PATH=$1
 
 # It then makes a list of all the directories in that folder to extract the names of all the Super-Kingdoms.
-LIST_ORDERS="Archaea Bacteria Eukaryota"
-# Then, we iterate over the Super-Kingdoms to execute the "Python_launcher.sh" for each family in each Super-Kingdom.
+LIST_ORDERS=$(cd $DATA_PATH | ls -d /*)
+# Then, we iterate over the Super-Kingdoms to execute the "Isolating_sequences_per_kingdom.py" for each family in each Super-Kingdom.
 for ORDER in $LIST_ORDERS; do
     
     echo $ORDER
+    # We make a local directory in which to save all the files with the extracted sequences.
+    mkdir $ORDER
     python3 Isolating_sequences_per_kingdom.py $ORDER $DATA_PATH
     
 done
