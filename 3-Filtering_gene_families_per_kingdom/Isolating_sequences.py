710697b22e9eab4972677b08642352cee20c4466
BasilePajot <91027316+BasilePajot@users.noreply.github.com>
2023-03-29 14:53:53 +0200

    Update Isolating_sequences.py

--- 3-Filtering_gene_families_per_kingdom/Isolating_sequences.py
+++ 3-Filtering_gene_families_per_kingdom/Isolating_sequences.py
@@ -3,20 +3,27 @@ import os as os
 import sys as sys
 import tqdm as tqdm
 
-# Taking into account all the variables that have been passed on by the launcher.
+# Taking into account all the variables that have been passed on by the launcher (the path to the data and the order to treat).
 order = sys.argv[1].strip("/")
 data_path = sys.argv[2]
+
+# We make a list of the files that are in the Super-Kingdom directory in the parallel folder.
 list_files = [file for file in os.listdir(os.path.join(data_path, order)) if file.endswith(".fasta")]
 
+# We open the reference file (file that contains all the sequence IDs from the considered Super-Kingdom) and making a list out of the lines.
 reference_file = [line.strip() for line in open("".join([os.path.join(data_path, order), "_sequences.mne"]), "r")]
+# In the same way, we make a list out of the file we created in the launcher. It contains all the sequence IDs that are to be checked in this script.
 file_sequences_families = [line.strip().replace(".", "_") for line in open("".join([os.path.join(data_path, order), "_sequences_in_families.txt"]), "r")]
 
+# We take the intersection of the two lists. It allows to check less sequence IDs.
 intersection_of_files = list(set(reference_file) & set(file_sequences_families))
 
+# We create a function that converts a list into a hash dictionnary that takes the hash of each sequence ID as a key.
 def Convert(lst):
     res_dct = {hash(lst[i]): i for i in range(len(lst))}
     return(res_dct)
 
+# We convert the intersected file into a hash dictionnary.
 hash_dict = Convert(intersection_of_files)
 
 # Create an extractor class that will be used to extract all the needed sequences and create new files with the extracted data.
@@ -28,7 +35,7 @@ class Extractor:
     
     # The "check_seqeunce" method checks if the read sequence ID is in the ".mne" file for each Super-Kingdom. 
     def check_sequence(self, line, hash_dict):
-        # If the line in the ".mne" file corresponds to a line in the gene-family file, it returns True and False otherwise.
+        # If the line that contains a sequence ID is in the hash dictionnary, it returns True and False otherwise.
         if hash(line.strip(">").strip().replace(".", "_")) in hash_dict:
             return(True)
         else:
@@ -54,14 +61,12 @@ class Extractor:
 extractor = Extractor()
 extractor.init()
 
-# Open the paths to the files. The first one is the file containing all the sequences for one family and the second
-# one contains all the sequence IDs for a Super-Kingdom.
-
-length_list_files = len(list_files)
+# We iterate over each file in the list of files we made earlier.
 for name_file_to_test in tqdm.tqdm(list_files):
 
+    # Each file to test is opened. 
     file_to_test = open(os.path.join(data_path, order, name_file_to_test), "r")
-    # Taking out the family name of the gene family we are treating here.
+    # Each gene family name is extracted to be used to name the newly-created files containing the sequences that are present in each Super-Kingdom.
     family_name = name_file_to_test.split("#")[1]
 
     # This loop iterates over each line in the gene family file. It tests several parameters. Each new sequence strats with ">". Therefore, for each
