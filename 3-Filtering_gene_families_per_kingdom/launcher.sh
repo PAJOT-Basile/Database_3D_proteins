d01a6a2da71fbb88e30cb1cd5e4d1814c0bcbc54
BasilePajot <91027316+BasilePajot@users.noreply.github.com>
2023-03-29 14:44:19 +0200

    Update launcher.sh

--- 3-Filtering_gene_families_per_kingdom/launcher.sh
+++ 3-Filtering_gene_families_per_kingdom/launcher.sh
@@ -6,6 +6,9 @@ LIST_ORDERS=$(cd $DATA_PATH | ls -d /*)
 # Then, we iterate over the Super-Kingdoms to execute the "Isolating_sequences_per_kingdom.py" for each family in each Super-Kingdom.
 for ORDER in $LIST_ORDERS; do
     
+    # We make a file containing all the sequences that are to be checked in the gene family files.
+    cat "${DATA_PATH}${ORDER}/*" | grep "^>" | cut -d">" -f2 | sort > "${DATA_PATH}${ORDER}_sequences_in_families.txt"
+    
     echo $ORDER
     # We make a local directory in which to save all the files with the extracted sequences.
     mkdir $ORDER
